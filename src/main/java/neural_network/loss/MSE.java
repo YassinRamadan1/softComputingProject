4f98af2beb541af48898a048f97244e94cdb667d
NourElbaz <nourelbaz123@gmail.com>
2025-12-15 20:12:42 +0200

    added: Loss Functions
    Implemented forward and gradient for Mean Squared Error and Binary Cross-Entopy

--- src/main/java/neural_network/loss/MSE.java
+++ src/main/java/neural_network/loss/MSE.java
@@ -1,4 +1,23 @@
 package neural_network.loss;
 
-public class MSE {
+public class MSE implements Loss {
+
+    @Override
+    public double forward(double[] yTarget, double[] yPred) {
+        double sum = 0.0;
+        for (int i = 0; i < yTarget.length; i++) {
+            double diff = yTarget[i] - yPred[i];
+            sum += diff * diff;
+        }
+        return sum / yTarget.length;
+    }
+
+    @Override
+    public double[] backward(double[] yTarget, double[] yPred) {
+        double[] gradient = new double[yTarget.length];
+        for (int i = 0; i < yTarget.length; i++) {
+            gradient[i] = 2 * (yPred[i] - yTarget[i]) / yTarget.length;
+        }
+        return gradient;
+    }
 }
