91ed76a5a231d504717104ba8ee135cd173f159c
NourEldinElbaz <nourelbaz123@gmail.com>
2025-12-15 18:35:29 +0200

    added: Activation functions

--- src/main/java/neural_network/activations/Linear.java
+++ src/main/java/neural_network/activations/Linear.java
@@ -1,4 +1,17 @@
 package neural_network.activations;
 
-public class Linear {
+public class Linear implements Activation {
+    @Override
+    public double forward(double x){
+        return x;
+    }
+
+    @Override
+    public double derivative(double x){
+        return 1;
+    }
+    @Override
+    public String getName(){
+        return "Linear";
+    }
 }
--- src/main/java/neural_network/activations/Tanh.java
+++ src/main/java/neural_network/activations/Tanh.java
@@ -1,4 +1,20 @@
 package neural_network.activations;
 
-public class Tanh {
+public class Tanh implements Activation {
+
+    @Override
+    public double forward(double x) {
+       return Math.tanh(x);
+    }
+
+    @Override
+    public double derivative(double x) {
+        double tanh = Math.tanh(x);
+        return 1 - tanh * tanh;
+    }
+
+    @Override
+    public String getName() {
+        return "Tanh";
+    }
 }
