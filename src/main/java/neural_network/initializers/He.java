96363e881382597c90fd12787cefb055194caeae
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2025-12-19 14:38:06 +0200

    fix init weights

--- src/main/java/neural_network/initializers/He.java
+++ src/main/java/neural_network/initializers/He.java
@@ -2,13 +2,14 @@ package neural_network.initializers;
 
 import java.util.Random;
 
-public class RandomUniform implements Initializer {
+public class He implements Initializer {
     private final Random random = new Random();
     @Override
     public double[] initializeWeights(int fanIn, int fanOut) {
+        double limit = Math.sqrt(2.0/fanIn);
         double[] weights = new double[fanIn];
         for (int i = 0; i < fanIn; i++) {
-            weights[i] = -0.05 + (0.1 * random.nextDouble());
+            weights[i] = -limit + (2 * limit * random.nextDouble());
         }
         return weights;
     }
--- src/main/java/neural_network/initializers/Xavier.java
+++ src/main/java/neural_network/initializers/Xavier.java
@@ -6,6 +6,7 @@ public class Xavier implements Initializer {
     private final Random random = new Random();
     @Override
     public double[] initializeWeights(int fanIn, int fanOut) {
+        // weights are stored per neuron so this is like weight[fanOut][fanIn].
         double limit = Math.sqrt(6.0 / (fanIn + fanOut));
         double[] weights = new double[fanIn];
         for (int i = 0; i < fanIn; i++) {
